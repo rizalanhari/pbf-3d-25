9d644ccf3c31d9e82da1b031568f959fbb46bf17
rizalanhari <rizalanhari6@gmail.com>
2021-02-28 19:30:08 +0700

    add laporan 2

---
+++ src/02_hello_world/hello/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Rizal Anhari (1841720218)
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/rizalanhari"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
