9dd3a173ff5daa75b99f5d55f2101544f604676a
rizalanhari <rizalanhari6@gmail.com>
2021-04-12 11:24:45 +0700

    add src doc 07

---
+++ src/07_context_hoc/theme-app/src/App.js
@@ -0,0 +1,13 @@
+import ThemeProvider from "./ThemeContext/ThemeProvider";
+import Menu from "./Menu";
+import SetColor from "./SetColor";
+
+export default function App() {
+  return (
+    <ThemeProvider>
+      <Menu />
+      <br />
+      <SetColor />
+    </ThemeProvider>
+  )
+}
