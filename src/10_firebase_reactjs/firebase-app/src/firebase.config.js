6dbcc75f9443e104ea8fd1af9dbad4277bf5f3a4
rizalanhari <rizalanhari6@gmail.com>
2021-05-03 13:22:14 +0700

    add src doc 10

---
+++ src/10_firebase_reactjs/firebase-app/src/firebase.config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDWNTkGWOgxgCJlzpHxj0IEjItArlflI90",
+    authDomain: "belajar-react-85e86.firebaseapp.com",
+    projectId: "belajar-react-85e86",
+    storageBucket: "belajar-react-85e86.appspot.com",
+    messagingSenderId: "244423492095",
+    appId: "1:244423492095:web:423b3470555c85a88467ca",
+    measurementId: "G-GK41X8X6LY"
+};
