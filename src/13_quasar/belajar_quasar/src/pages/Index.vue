8ad4a38dcda93333b203aa4505261574bac8d867
rizalanhari <rizalanhari6@gmail.com>
2021-06-07 16:12:08 +0700

    update laporan 13

---
+++ src/13_quasar/belajar_quasar/src/pages/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page class="flex flex-center">
+    <h3>{{ hello }}</h3>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data() {
+    return {
+      hello: "Halo, Nama Saya Rizal Anhari dengan NIM 1841720218"
+    }
+  }
+}
+</script>
